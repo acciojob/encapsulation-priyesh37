9c6711780ed454caca4aace7fb007ba27c507e5e
Priyesh <priyeshthakker31@gmail.com>
2023-11-27 22:54:20 +0530

    change both files

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+
+	public static void main(String[] args){
+
+		RWOnly obj = new RWOnly();
+		 //   obj.name = "xyz";
+    //   System.out.println(obj.name);
+        obj.setName("Shubham");
+        System.out.println(obj.getName());
+
+	}
   
 }
+/*
+ * 
+ * Exception in thread "main" java.lang.Error: Unresolved compilation problems: 
+        The field RWOnly.name is not visible
+        The field RWOnly.name is not visible
+
+        at com.driver.Main.main(Main.java:6)
+ * 
+*/
